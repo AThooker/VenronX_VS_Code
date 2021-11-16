0c9f6d775eccccdb2c185d524eb8e00eac150190
Austin Hooker <austin.hooker@ruralsource.com>
2021-11-16 17:00:15 -0500

    Unit test for oil pressure updated, still need to run tests

--- GaugeLibraryTest/OilPressureTests.cs
+++ GaugeLibraryTest/OilPressureTests.cs
@@ -1,3 +1,5 @@
+using System;
+using CustomExceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VenronX_Gauges;
 
@@ -13,6 +15,31 @@ namespace GaugeLibraryTest
         {
             _oil = new OilPressure();
         }
+        //***OIL PRESSURE TESTS***//
 
+        //get min/max oil pressure
+        [TestMethod]
+        public void TestOilPressureValues()
+        {
+            var minMaxOil = _oil.OilPressureMinMax;
+            Assert.AreEqual(minMaxOil, new Tuple<int, int>(5, 65));
+        }
+        //get user set values for min/max oil pressure
+        [TestMethod]
+        public void TestOilPressureUserSetValues()
+        {
+            _oil.Min = 10;
+            _oil.Max = 70;
+            var userGauge = _oil.UserOilPressureMinMax;
+            Assert.AreEqual(userGauge, new Tuple<int, int>(10,70));
+        }
+        //set pressure > max value and expect custome exception (output engine shutdown)
+        [TestMethod]
+        [ExpectedException(typeof(OilPressureAtMaxException))]
+        public void GetExceptionOilPressureAtMax()
+        {
+            _oil.Pressure = 80;
+            _oil.WarnsEngineToShutDown();
+        }
     }
 }
